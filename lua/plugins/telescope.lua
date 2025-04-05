57d03817fa4f8e97dad3a91fe8b14d8c2fb13ea3
Mateus-Lacerda <mateuslacerda253@gmail.com>
2025-04-05 02:26:47 -0300

    Feat: Clangd LSP, telescope config to view SOME hidden files and copilot mappings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,15 @@
 return {
-    'nvim-telescope/telescope.nvim', branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' }
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+        defaults = {
+            file_ignore_patterns = { '.git/', 'node_modules/', '.venv/' },
+        },
+        pickers = {
+            find_files = {
+                hidden = true,
+            },
+        }
     }
+}
