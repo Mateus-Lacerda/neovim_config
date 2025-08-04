ef3337f835b8ea07370e5d76bc2bbcccda15e775
Mateus-Lacerda <mlacerdam.ai@gmail.com>
2025-08-04 11:22:59 -0300

    feat: java

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -275,6 +275,7 @@
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
       })
+
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
